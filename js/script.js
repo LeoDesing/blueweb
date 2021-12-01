ab662b0acc2dbc68648645bf4bbf044662412ad1
LeoDesing <95183849+LeoDesing@users.noreply.github.com>
2021-12-01 13:37:11 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let menu = document.getElementById("menu");
+let responsive = document.getElementById("responsive");
+
+responsive.addEventListener("click", function () {
+    menu.classList.toggle("toggle-btn");
+})
